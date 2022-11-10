d52fa6f409c032e407926dcd411ce7c856b2d98f
nnlawrence <nathanielawrence@gmail.com>
2022-11-10 16:33:42 -0700

    Delete LoginPageAutomated/spec/support/modules/google directory

--- LoginPageAutomated/spec/support/modules/google/google_shared_methods.rb
+++
@@ -1,9 +0,0 @@
-module Modules
-  module Google
-    module GoogleSharedMethods
-      def some_shared_method
-        puts 'this method will be shared across all google tests'
-      end
-    end
-  end
-end
